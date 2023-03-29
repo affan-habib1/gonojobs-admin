dee2901baadc7f5cd6d827e4f1bff4426b0efdf0
affan-habib1 <affan.gononet@gmail.com>
2023-03-29 13:00:44 +0600

    urls udpate

--- src/helpers/UrlBuilder.js
+++ src/helpers/UrlBuilder.js
@@ -4,17 +4,15 @@ class UrlBuilderHelper {
   }
 
   coreServiceApi(path) {
-    //return `http://192.168.1.37:8000/api/v1/${path}`;
     return `http://13.212.68.212:5001/api/v1/${path}`;
   }
-
+  
   jobServiceApi(path) {
-    // return `http://103.4.145.250:8028/api/v1/${path}`;
-    return `http://192.168.1.211:8028/api/v1/${path}`;
+    return `http://13.212.68.212:5002/api/v1/${path}`;
   }
-
+  
   resumeServiceApi(path) {
-    return `http://103.4.145.251:4028/${path}`;
+    return `http://13.212.68.212:5003/api/v1/${path}`;
   }
 }
 
